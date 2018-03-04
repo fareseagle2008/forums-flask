70c6bd4a9fb9207bea992d38275c6f6b9666d43f
Fares AbuAmin <fareseagle2012@gmail.com>
2018-03-04 13:31:00 +0200

    deployment1.2

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from app import stores, models, dummy_data
-from app.views import *
+
+
 
 app = Flask(__name__)
+
 
 member_store = stores.MemberStore()
 post_store = stores.PostStore()
 dummy_data.seed_stores(member_store, post_store)
+
+from app import views
 
